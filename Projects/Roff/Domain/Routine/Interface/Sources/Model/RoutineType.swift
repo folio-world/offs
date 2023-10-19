5c204f8d7f8fd1a79f402fae4749e6c8868a1fe4
송영모 <mooyoung2309@gmail.com>
2023-10-19 22:28:48 +0900

    feat: roff calendar 이동

--- Projects/Roff/Domain/Routine/Interface/Sources/Model/RoutineType.swift
+++ Projects/Roff/Domain/Routine/Interface/Sources/Model/RoutineType.swift
@@ -9,4 +9,18 @@ import Foundation
 
 public enum RoutineType {
     case health
+    case learning
+    case productivity
+    case wellBeing
+    case social
+    case finance
+    case entertainment
+    case food
+    case hobbies
+    case relationships
+    case travel
+    case sustainability
+    case style
+    case business
+    case custom
 }
