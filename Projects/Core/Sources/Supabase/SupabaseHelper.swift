aba0c5030716b2633b72ada25c1c2ad4f1be3cab
송영모 <mooyoung2309@gmail.com>
2024-02-04 14:01:00 +0900

    feat: supabase client 추가

--- Projects/Core/Sources/Supabase/SupabaseHelper.swift
+++ Projects/Core/Sources/Supabase/SupabaseHelper.swift
@@ -16,6 +16,36 @@ public struct SupabaseHelper {
         supabaseURL: URL(string: Environment.supabaseProjectURL)!,
         supabaseKey: Environment.supabaseAPIpublicKey
     )
+}
+
+func countNodes(in tree: Node, matching predicate: (Node) throws -> Bool) rethrows -> Int {
+  class MyNodeVisitor: NodeVisitor {
+    var error: (any Error)? = nil
+    var count: Int = 0
+    var predicate: (Node) throws -> Bool
+
+    init(predicate: @escaping (Node) throws -> Bool) {
+      self.predicate = predicate
+    }
     
-    static func getInvestments() { }
+    override func visit(node: Node) {
+      do {
+        if try predicate(node) {
+          count = count + 1
+        }
+      } catch let localError {
+        error = error ?? localError
+      }
+    }
+  }
+  
+  return try withoutActuallyEscaping(predicate) { predicate in
+    let visitor = MyNodeVisitor(predicate: predicate)
+    visitor.visitTree(node)
+    if let error = visitor.error {
+      throw error // error: is not throwing as a consequence of 'predicate' throwing.
+    } else {
+      return visitor.count
+    }
+  }
 }
